e179d43b89d0d34e0725277c85fb90d013d53bbc
naoto67 <uver.com67@gmail.com>
2020-07-20 18:27:02 +0900

    画像の初期化処理追加

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -208,6 +208,7 @@ func getInitialize(c echo.Context) error {
 	db.MustExec("DELETE FROM channel WHERE id > 10")
 	db.MustExec("DELETE FROM message WHERE id > 10000")
 	db.MustExec("DELETE FROM haveread")
+	initImages()
 	return c.String(204, "")
 }
 
--- webapp/go/src/isubata/util.go
+++ webapp/go/src/isubata/util.go
@@ -10,3 +10,24 @@ const PUBLIC_DIR = "/home/isucon/isubata/webapp/public/icons/"
 func writeAvatarIcon(filename string, data []byte) error {
 	return ioutil.WriteFile(fmt.Sprintf("%s%s", PUBLIC_DIR, filename), data, 0666)
 }
+
+type image struct {
+	Name string `db:"name"`
+	Data []byte `db:"data"`
+}
+
+func initImages() error {
+	var images []image
+	err := db.Select(&images, "SELECT * FROM images")
+
+	if err != nil {
+		return err
+	}
+
+	for _, v := range images {
+		if err = writeAvatarIcon(v.Name, v.Data); err != nil {
+			return err
+		}
+	}
+	return nil
+}
